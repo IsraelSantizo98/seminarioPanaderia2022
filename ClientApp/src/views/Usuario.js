73fa483a5dbd37acefe2a0ee137c45d8555060e7
Jose Israel Santizo Santos <joseisraelss.jiss@gmail.com>
2022-10-19 21:30:31 -0600

    Se modifica codigo prueba pipeline

--- ClientApp/src/views/Usuario.js
+++ ClientApp/src/views/Usuario.js
@@ -149,7 +149,7 @@ const Usuario = () => {
     const cerrarModal = () => {
         setUsuario(modeloUsuario)
         setVerModal(!verModal);
-        //cod
+        //code
     }
 
     const guardarCambios = async () => {
